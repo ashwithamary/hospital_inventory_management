537651b2c731395746439d40c613faf5423a770d
Mary Ashwitha Gopu <ashwithamary@gmail.com>
2025-02-10 14:02:21 -0500

    Updated MongoDB console logs

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true 
+  credentials: true
 }));
 
 app.use(express.json());
@@ -26,10 +26,10 @@ const io = new Server(httpServer, {
   cors: {
     origin: [
       'http://localhost:3000',                          
-      'https://hospital-inventory-management.vercel.app' 
+      'https://hospital-inventory-management.vercel.app'  
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true  
+    credentials: true
   }
 });
 
@@ -78,9 +78,4 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log('CORS origins:', app.get('_cors').origin); 
-});
-
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Promise Rejection:', err);
 });
